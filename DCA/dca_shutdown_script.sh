f828a9da0ac4e130c50bc5d538ceb68916fee1c4
Marc Paquette <paquem@Marcs-MacBook-Pro.local>
2016-02-26 12:09:48 -0500

    Moved files

---
+++ DCA/dca_shutdown_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ssh mdw  #get on the master
+su - gpadmin  #Make sure you are gpadmin
+gpstart -a #make sure the database is up
+psql -t -c "select distinct hostname from gp_segment_configuration where hostname not like 'mdw%'" template1 > ~/host_shutdown #generate host file
+gpstop -af # stop database before system shutdown
+su - #log in as root
+source /usr/local/greenplum-db/greenplum_path.sh # source the greenplum environment
+pssh -f ~gpadmin/host_shutdown -e "shutdown -h now" # shutdown all segment servers except for mdw
+shutdown -h now # shutdown mdw
+
