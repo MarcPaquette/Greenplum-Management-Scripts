e9384c171fa54311743012b6bc9112744530431b
MarcPaquette <mpaquette@pivotal.io>
2016-02-26 12:10:47 -0500

    Delete battery_health_check.sh

--- battery_health_check.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-omconfig storage controller action=exportlog controller=0; cat /var/log/lsi_`date +%m%d`.log | grep -A 8 'BATTERY MONITORED INFORMATION' |tail -8| cut -c 6- | grep "Full";
-cat /var/log/lsi_`date +%m%d`.log | grep 'Manufacturer Name' | head -n 1
