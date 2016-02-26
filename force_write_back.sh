e6e6b2674cbea09ddc5211755658f6c5d0a01979
MarcPaquette <mpaquette@pivotal.io>
2016-02-26 12:11:40 -0500

    Delete force_write_back.sh

--- force_write_back.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-omreport storage vdisk |grep Write
-for i in 0 1 2 3; do omconfig storage vdisk controller=0 vdisk=$i action=changepolicy writepolicy=fwb; done
-omreport storage vdisk |grep Write
-
